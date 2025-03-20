433155c13f19b379e89b314135b1c5a7d093b6f1
Thomas Nicholas <tcnicholas@me.com>
2025-03-20 18:48:25 +0100

    bump 0.1.2 -> 0.1.3

--- src/ase_hdf5/__init__.py
+++ src/ase_hdf5/__init__.py
@@ -2,4 +2,4 @@ from ase_hdf5.core import ASEH5Trajectory
 from ase_hdf5.utils import print_file_size
 
 __all__ = ["ASEH5Trajectory", "print_file_size"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
